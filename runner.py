1c8a540df4bb88e74e2cec46132922c7cbc0fd51
Letlhogonolo Segoe <noloxtreme@gmail.com>
2020-04-25 19:08:03 +0200

    Reverse navigation

--- runner.py
+++ runner.py
@@ -1,5 +1,11 @@
+import numpy as np
 from roman_numerals import RomanNumerals
 
 if __name__ == "__main__":
     numerals = RomanNumerals()
-    print(numerals.integer_to_numerals(1066))
+
+    arr = np.arange(1, 10, dtype='i')
+    arr = arr[::-1]
+
+    for i in arr:
+        print('{} = {}'.format(i, numerals.integer_to_numerals(int(i))))
